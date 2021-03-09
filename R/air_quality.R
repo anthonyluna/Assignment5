b1bf5fb08266748c8f006205623167120dd40095
alexehrens <alexehrens@ucsb.edu>
2021-03-08 18:39:29 -0800

    created documentation for both air quality and interest functions

--- R/air_quality.R
+++ R/air_quality.R
@@ -1,9 +1,27 @@
-#### Function #2 - using case_when() in a function
-
-## air quality index for particulate matter
-
-# parameters - pm2.5_conc = concentration of particulate matter of in the air
-### NOTE - thresholds for air quality index levels are specific to the United States Air Quality Index (AQI), this function does not apply to other countries
+#' air_quality - a function for classifying air quality levels
+#'
+#' Purpose: to determine the air quality index level given a 24-hour
+#' concentration of particulate matter (PM2.5). This function uses the United
+#' States Air Quality Index (AQI), which determines levels of concern for
+#' concentrations of the 5 major air quality pollutants. This application of the
+#' AQI classification is specific to 24-hour average PM2.5 concentrations. PM2.5
+#' is fine particulate matter that is smaller than 2.5 micrometers, small enough
+#' to be inhaled into the lungs and bloodstream and cause long-term health
+#' effects.
+#'
+#' NOTE: thresholds for AQI levels in this function are specific to the United
+#' States, this function does not apply to other countries with different index
+#' thresholds
+#'
+#' @source AQI index values and levels: https://www.airnow.gov/aqi/aqi-basics/
+#' @source AQI technical assistance document - index thresholds for PM2.5
+#'   concentrations found in Table 4 on pages 4-5:
+#'   https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf
+#'
+#' @param pm2.5_conc the concentration of fine particulate matter (PM2.5) in the
+#'   air. Concentrations are reported as a 24-hour average, in micrometers per
+#'   cubic meter (μg/m^3). The AQI index thresholds for PM2.5 concentrations
+#'   were taken from Table 4 of the AQI technical assistance document
 
 air_quality_index <- function(pm2.5_conc){
   # error_checking
@@ -11,13 +29,13 @@ air_quality_index <- function(pm2.5_conc){
 
   # determine air quality index levels based on concentrations
   AQ_index <- case_when(
-    pm2.5_conc <= 50 ~ "Good",
-    pm2.5_conc > 50 & pm2.5_conc <= 100 ~ "Moderate",
-    pm2.5_conc > 100 & pm2.5_conc <= 150 ~ "Unhealthy for Sensitive Groups",
-    pm2.5_conc > 150 & pm2.5_conc <= 200 ~ "Unhealthy",
-    pm2.5_conc > 200 & pm2.5_conc <= 300 ~ "Very Unhealthy",
-    pm2.5_conc > 300 & pm2.5_conc <= 500 ~ "Hazardous",
-    pm2.5_conc > 500 ~ "Beyond U.S. AQ Index"
+    pm2.5_conc <= 12 ~ "Good",
+    pm2.5_conc > 12 & pm2.5_conc <= 35.4 ~ "Moderate",
+    pm2.5_conc > 35.4 & pm2.5_conc <= 55.4 ~ "Unhealthy for Sensitive Groups",
+    pm2.5_conc > 55.4 & pm2.5_conc <= 150.4 ~ "Unhealthy",
+    pm2.5_conc > 150.4 & pm2.5_conc <= 250.4 ~ "Very Unhealthy",
+    pm2.5_conc > 250.4 & pm2.5_conc <= 500.4 ~ "Hazardous",
+    pm2.5_conc > 500.4 ~ "Beyond U.S. AQ Index"
   )
 
   return(AQ_index)
--- R/interest_while_fun.R
+++ R/interest_while_fun.R
@@ -1,26 +1,38 @@
-#### Function #1 - using a while() loop
-
-### compound interest function
-
-# parameters:
-## balance = the amount of money in a bank account, when setting this parameter you are setting the amount of initial balance, function produces final balance after applying compound interest for X years
-## interest_rate = the rate at which interest is being applied, set to 0.02 but could be changed manually
-## compound_per_yr = the amount of times interest is compounded per year, this should be set to just 1 most of the time but kept it as variable in case that is not the case
-## yr = number of years since your initial balance has started to gain interest, starting with year 0 as the first year 
-
-# compound interest formula:
-### Final Amount = initial_balance*(1 + (interest_rate/number_of_times_compounded_per_year))^(number_of_times_compounded_per_year*years)
-
+#'interest_fun - a while loop function for calculating compound interest
+#'
+#'Purpose: to calculate the final balance in a bank account and amount of years
+#'it would take to reach a certain target in an account where an initial
+#'investment earns compound interest. The function uses the compound interest
+#'formula and contains a `while()` loop which will continue to apply interest on
+#'a given investment until a given target balance is reached. It will report the
+#'amount of years it would take to reach that target balance and the actual
+#'final balance that is achieved at the end of the year in which the target was
+#'reached.
+#'
+#'@source compound interest formula:
+#'  https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php#:~:text=Compound%20interest%2C%20or%20'interest%20on,the%20number%20of%20time%20periods
+#'
+#'@param balance the amount of money in a bank account. When setting this
+#'  parameter, you are setting the amount of initial balance. The function then
+#'  produces the final balance after applying compound interest for X years
+#'@param interest_rate the rate at which interest is being applied
+#'@param compound_per_yr the amount of times the balance is compounded per year.
+#'  This typically should be set to 1 most of the time, but can be changed by
+#'  user in situations where that is not the case
+#'@param yr number of years since the initial investment was made, the amount of
+#'  time that balance has had time to gain interest. Starts with year 0 as the
+#'  first year
+#'
 
 interest_fun <- function(balance, target, interest_rate, compound_per_yr, yr){
   # error checking - make sure inputs are positive
   if(balance < 0 | interest_rate < 0 | compound_per_yr < 0 | yr < 0) return("Conditions must all be positive!")
-  
+
   while((balance < target) && (yr < 1000)){
-  
+
   # calculate amount of money in account each year using compound interest formula
-  balance = balance*(1 + (interest_rate/compound_per_yr))^(compound_per_yr*yr)
-  
+  balance = balance * (1 + (interest_rate / compound_per_yr)) ^ (compound_per_yr * yr)
+
   # count the number of years
   yr = yr+1
   }
