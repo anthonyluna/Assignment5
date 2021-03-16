0c11a20588f84ff0a95d4d2b870c7f085d9656dc
alexehrens <alexehrens@ucsb.edu>
2021-03-16 12:46:39 -0700

    set up file structure for testing and wrote interest function test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(Assignment5)
+
+test_check("Assignment5")
---
+++ tests/testthat/interest_while_test.R
@@ -0,0 +1,8 @@
+test_that("interest_fun_works",{
+  # test that final balance ends up being greater than target
+  target = 100000
+  expect_true(interest_fun(target, balance = 1, yr = 0, compound_per_yr = 1, interest_rate = 0.02)$balance > target)
+
+  # write a test that is expected to fail (balance is supposed to be > target)
+  expect_true(interest_fun(target, balance = 1, yr = 0, compound_per_yr = 1, interest_rate = 0.02)$balance < target)
+})
