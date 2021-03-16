9507d72c232c536cf987022b784db3cfb7a17f81
alexehrens <alexehrens@ucsb.edu>
2021-03-16 12:59:25 -0700

    wrote air quality function test

---
+++ tests/testthat/air_quality_test.R
@@ -0,0 +1,11 @@
+# create vector of sample PM2.5 concentration data
+pm2.5_data <- c(16, 102, 314, 501)
+
+test_that("air_quality_index_works", {
+  # test that each value in sample vector match with expected index level
+  expect_match(air_quality_index(pm2.5_data[1]), "Moderate")
+  expect_match(air_quality_index(pm2.5_data[2]), "Unhealthy")
+  expect_match(air_quality_index(pm2.5_data[3]), "Hazardous")
+  expect_match(air_quality_index(pm2.5_data[4]), "Beyond U.S. AQ Index")
+}
+          )
